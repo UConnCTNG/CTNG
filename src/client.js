f0f2bf69403c5a3fd12a3d25865ef415c454abb2
Kevin Freyberg <kfreyberg01@gmail.com>
2023-02-07 14:28:06 -0500

    skeleton code for client-browser func

---
+++ src/client.js
@@ -0,0 +1,30 @@
+class Client {
+  constructor(params) {
+    //
+  }
+
+  listenForCertificates(website_name = null) {
+    // background process
+  }
+
+  static getCertificateFromStorage() {
+    // returns cert object
+  }
+
+  static parseCertificate() {
+    // returns status (1 if successfully stored)
+  }
+
+  static storeCertificate() {
+    // returns status (1 if successfully stored)
+  }
+
+  static isCertificateStored() {
+    // returns bool
+  }
+
+  static deleteCertificateFromStorage() {
+    // returns status
+  }
+
+}
