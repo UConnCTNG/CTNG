b4286682ee7098cfac2e1c56fb95654e3fbef15c
Kevin Freyberg <kfreyberg01@gmail.com>
2023-02-07 16:16:58 -0500

    Background listener for certs

--- background.js
+++ background.js
@@ -1,19 +1,19 @@
-var log = console.log.bind(console)
+import Client from "./src/client.js";
 
-log(`\n\nTLS browser extension loaded`)
-var arr = { urls: ["https://github.com/"]}
-// ['<all_urls>']
+var log = console.log.bind(console)
+var github_sites = { urls: ["https://github.com/"]}
 
 // https://developer.chrome.com/extensions/match_patterns
 var ALL_SITES = { urls: ['<all_urls>'] }
-
+var client = new Client(log)
 
 // Mozilla doesn't use tlsInfo in extraInfoSpec 
-var extraInfoSpec = ['blocking']; 
+var extraInfoSpec = ['blocking', "responseHeaders"]; 
 
+log("Certificate listener initialized.")   
 // https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/webRequest/onHeadersReceived
 browser.webRequest.onHeadersReceived.addListener(async function(details){
-    log(`\n\nGot a request for ${details.url} with ID ${details.requestId}`)
+    //log(`\n\nGot a request for ${details.url} with ID ${details.requestId}`)
 
     // Yeah this is a String, even though the content is a Number
     var requestId = details.requestId
@@ -23,8 +23,6 @@ browser.webRequest.onHeadersReceived.addListener(async function(details){
         rawDER: false
     });
 
-    log(`securityInfo: ${JSON.stringify(securityInfo, null, 2)}`)
+    client.parseCertificate(securityInfo)
 
 }, ALL_SITES, extraInfoSpec) 
-
-log('Added listener')
--- src/client.js
+++ src/client.js
@@ -1,18 +1,35 @@
-class Client {
-  constructor(params) {
-    //
-  }
-
-  listenForCertificates(website_name = null) {
-    // background process
+export default class Client {
+  
+  constructor(sites, log) {
+    // https://developer.chrome.com/extensions/match_patterns
+    this.sites = sites; 
+    this.log = console.log.bind(console)
   }
 
   static getCertificateFromStorage() {
     // returns cert object
   }
 
-  static parseCertificate() {
-    // returns status (1 if successfully stored)
+  parseCertificate(securityInfo) {
+    var cert = securityInfo.certificates[0]
+    var subjectString = cert.subject
+    var certInfo = {
+      SerialNumber: cert.serialNumber,
+      Subject: { // need to parse this string, "CN=*.nr-data.net,O=\"New Relic, Inc.\",L=San Francisco,ST=California,C=US"
+        Organization: "",
+        CommonName: "",
+        Country: ""
+      },
+      Validity: { Start: new Date(cert.validity.start), End: new Date(cert.validity.end) },
+      IsCA: true,
+      KeyUsage: "",
+      ExtKeyUsage: "",
+      BasicConstraintsValid: true
+    }
+
+    this.log(certInfo)
+
+    // storeCertificate(certInfo)
   }
 
   static storeCertificate() {
