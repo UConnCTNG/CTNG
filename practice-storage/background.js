4fd094e3d065af26e9216effb01dfcbaeedb2d70
valeriefonsecalopes <124641367+valeriefonsecalopes@users.noreply.github.com>
2023-03-19 22:30:43 -0400

    stored sths correctly

--- practice-storage/background.js
+++ practice-storage/background.js
@@ -1,8 +1,67 @@
-fetch('http://localhost:3000/ClientUpdateTest')
-    .then(function(resp) {
-        return resp.json();
-    })
+// currently stores the sths correctly...
+console.log("hi is this working?");
+
+fetch("http://localhost:3000/STHs")
+    .then(response => response.json())
     .then(function(data) {
+        
+        // creates sth array
+        var sths = [];
+
+        // orig data
+        console.log(data);
+
+        // stores the actual sths into the array that was just made
+        for (var i = 0; i < data.length; i++) {
+            sths.push(data[i]); 
+        }
+        
+        // check if exact same as orig data
+        console.log(sths);
+
+        // store sths
+        browser.storage.local.set({sths});
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // jobin's parsing code
+        //var parsedUpdate = JSON.parse(data);
+        //var parsedUpdate = update;
+        /*
+        
+        //var poms = [];
+        var revs = [];
+        for (var i = 0; i < Object.keys(parsedUpdate.STHs).length; i++) {
+            sths.push(parsedUpdate.STHs[i]); 
+        }
+        for (var j = 0; j < Object.keys(parsedUpdate.REVs).length; j++) {
+            revs.push(parsedUpdate.REVs[j]);
+        }
+       
+       // for (var k = 0; k< Object.keys(parsedUpdate.PoMs).length; k++) {
+         //   poms.push(parsedUpdate.PoMs[k]);
+        //}
+    
+       */ 
+
+
+    /*
         console.log(data);
         console.log(data.REVs);
         console.log(data.PoMs);
@@ -26,6 +85,9 @@ fetch('http://localhost:3000/ClientUpdateTest')
         browser.storage.local.set({PoMsig});
         */
 
+
+        // storing of something
+        /*
         let results = browser.storage.local.get(data);
         results.then(function(result) {
             console.log(result);
@@ -35,5 +97,4 @@ fetch('http://localhost:3000/ClientUpdateTest')
         let results2 = browser.storage.local.get(data);
         results2.then(function(result) {
             console.log(result);
-        });
-    });
+        });*/
