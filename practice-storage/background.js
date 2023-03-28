06b5036517acd3338b1322f2cb071ab9faa26cdb
Riana Soliven <79773286+rianasoliven@users.noreply.github.com>
2023-03-27 20:52:19 -0400

    Co-authored-by: Valerie Fonseca-Lopes <valerieflopes@users.noreply.github.com>

--- practice-storage/background.js
+++ practice-storage/background.js
@@ -1,128 +1,151 @@
+// test with for loop
+// var sths = 'sths';
+// var timestamps = 'timestamps'
+// for(var p = 0; p < 4; p++) {
+//     theUrl = 'http://localhost:3000/?period=' + p
+//     fetch(theUrl)
+//         .then(response => response.text())
+//         .then((text) => {
+//             console.log(p)
+//             // getting the payload as an Object 
+//             const data = JSON.parse(text);
 
+//             //var sths0 = [] // store the sths from period=0 here
+//             eval('var ' + sths + p + '= [];');
+
+//             //var timestamps0 = [] // store the timestamp from period=0 
+//             eval('var ' + timestamps + p + '= [];');
+
+//             for (var i = 0; i < Object.keys(data.STHs).length; i++) {
+//                 //sths0.push(data.STHs[i]);
+//                 eval(sths + p + '.push(data.STHs[i]);')
+
+//                 //timestamps0.push(data.STHs[i].timestamp);
+//                 eval(timestamps + p + '.push(data.STHs[i].timestamp);')
+//             }
+
+//             // store the period=0 sths and timestamps into local storage
+//             //browser.storage.local.set({sths0 /*key name in local storage*/});
+//             eval('browser.storage.local.set({' + sths + p + '});')
+
+//             //browser.storage.local.set({timestamps0 /*key name in local storage*/})
+//             eval('browser.storage.local.set({' + timestamps + p + '})')
+            
+//             // checks
+//             console.log("The following is the STH(s) from period " + p + ":")
+
+//             //console.log(sths0);
+//             eval('console.log(' + sths + p + ');')
+
+//             //console.log(timestamps0);
+//             eval('console.log(' + timestamps + p + ');')
+//         })
+// }
+
+// if period=0
 fetch('http://localhost:3000/?period=0')
   .then(response => response.text())
   .then(text => {
+    // getting the payload as an Object 
     const data = JSON.parse(text);
-    console.log(typeof data);
 
-    console.log("The following data is the original payload:")
-    console.log(data);
-
-    var sths = []
+    var sths0 = [] // store the sths from period=0 here
+    var timestamps0 = [] // store the timestamp from period=0 
+    var revs0 = []
     for (var i = 0; i < Object.keys(data.STHs).length; i++) {
-        sths.push(data.STHs[i]);
+        sths0.push(data.STHs[i]);
+        timestamps0.push(data.STHs[i].timestamp);
+        revs0.push(data.REVs[i]);
     }
-    console.log("The following data is the STHs array:")
-    console.log(sths);
 
-    browser.storage.local.set({sths});
+    // store the period=0 sths and timestamps into local storage
+    browser.storage.local.set({sths0 /*key name in local storage*/});
+    browser.storage.local.set({timestamps0 /*key name in local storage*/})
+    browser.storage.local.set({revs0})
+    
+    // checks
+    console.log("The following is the STH(s) from period 0:")
+    console.log(sths0);
+    console.log(timestamps0);
 
+    console.log("The following is the REV(s) from period 0:")
+    console.log(revs0)
   });
 
+// if period=1
+fetch('http://localhost:3000/?period=1')
+  .then(response => response.text())
+  .then(text => {
+    // getting the payload as an Object 
+    const data = JSON.parse(text);
 
+    var sths1 = [] // store the sths from period=1 here
+    var timestamps1 = [] // store the timestamp from period=1 
+    var revs1 = []
+    for (var i = 0; i < Object.keys(data.STHs).length; i++) {
+        sths1.push(data.STHs[i]);
+        timestamps1.push(data.STHs[i].timestamp);
+        revs1.push(data.REVs[i]);
+    }
 
-/*fetch("http://localhost:3000/?period=0").then((response) => {
-    console.log(response.type);
+    // store the period=1 sths and timestamps into local storage
+    browser.storage.local.set({sths1 /*key name in local storage*/});
+    browser.storage.local.set({timestamps1 /*key name in local storage*/})
+    browser.storage.local.set({revs1})
 
-    const contentType = response.headers.get("content-type");
-    console.log(contentType);
-    console.log(response)
-
-    const obj = JSON.parse(JSON.stringify(response));
-
+    // check sths
+    console.log("The following is the STH(s) from period 1:")
+    console.log(sths1);
   });
-*/
 
+// if period=2
+fetch('http://localhost:3000/?period=2')
+  .then(response => response.text())
+  .then(text => {
+    // getting the payload as an Object 
+    const data = JSON.parse(text);
 
+    var sths2 = [] // store the sths from period=0 here
+    var timestamps2 = [] // store the timestamp from period=2 
+    var revs2 = []
+    for (var i = 0; i < Object.keys(data.STHs).length; i++) {
+        sths2.push(data.STHs[i]);
+        timestamps2.push(data.STHs[i].timestamp);
+        revs2.push(data.REVs[i]);
+    }
 
-/*
-// currently stores the sths correctly...
-// console.log("hi is this working?");
+    // store the period=2 sths and timestamps into local storage
+    browser.storage.local.set({sths2 /*key name in local storage*/});
+    browser.storage.local.set({timestamps2 /*key name in local storage*/})
+    browser.storage.local.set({revs2})
 
-fetch("http://localhost:3000/?period=0") // have to specify each url to access a certain resource 
-    .then(response => response.json())
-    .then(function(data) {
-        
-        // creates sth array (jobin has this)
-        //var sths = [];
+    // check sths
+    console.log("The following is the STH(s) from period 2:")
+    console.log(sths2);
+});
 
-        // check orig data
-        console.log("The following data is the original payload:")
-        console.log(data);
-        //console.log(data.type);
-        const contentType = data.headers.get("content-type");
-        console.log(contentType);
+// if period=3
+fetch('http://localhost:3000/?period=3')
+  .then(response => response.text())
+  .then(text => {
+    // getting the payload as an Object 
+    const data = JSON.parse(text);
 
-        var parsedUpdate = JSON.parse(data);
-        console.log(parsedUpdate.type);
-        // stores the actual sths into the array that was just made (jobin has this)
-        /*for (var i = 0; i < data.length; i++) {
-            sths.push(data.STHs[i]); 
-        }
-        
-        // check if sths array is exact same as orig data
-        console.log("The following data is the STHs array:")
-        console.log(sths);
+    var sths3 = [] // store the sths from period=0 here
+    var timestamps3 = [] // store the timestamp from period=3
+    var revs3 = []
+    for (var i = 0; i < Object.keys(data.STHs).length; i++) {
+        sths3.push(data.STHs[i]);
+        timestamps3.push(data.STHs[i].timestamp);
+        revs3.push(data.REVs[i]);
+    }
 
-        // store sths into local storage (local to the machine that the extension is installed on)
-        browser.storage.local.set({sths});*/
-    
+    // store the period=3 sths and timestamps into local storage
+    browser.storage.local.set({sths3 /*key name in local storage*/});
+    browser.storage.local.set({timestamps3 /*key name in local storage*/})
+    browser.storage.local.set({revs3})
 
-        // jobin's parsing code
-        //var parsedUpdate = JSON.parse(data);
-        //var parsedUpdate = update;
-        /*
-        
-        //var poms = [];
-        var revs = [];
-        for (var i = 0; i < Object.keys(parsedUpdate.STHs).length; i++) {
-            sths.push(parsedUpdate.STHs[i]); 
-        }
-        for (var j = 0; j < Object.keys(parsedUpdate.REVs).length; j++) {
-            revs.push(parsedUpdate.REVs[j]);
-        }
-       
-       // for (var k = 0; k< Object.keys(parsedUpdate.PoMs).length; k++) {
-         //   poms.push(parsedUpdate.PoMs[k]);
-        //}
-    
-       */ 
-
-
-    /*
-        console.log(data);
-        console.log(data.REVs);
-        console.log(data.PoMs);
-        console.log(data.MonitorId);
-        console.log(data.Period);
-        console.log(data.PoMsig);
-        
-        const STH = data.STHs
-        const REV = data.REVs
-        const PoM = data.PoMs
-        const MonitorId = data.MonitorId
-        const Period = data.Period
-        const PoMsig = data.PoMsig
-        
-        browser.storage.local.set({data});
-        /*
-        browser.storage.local.set({REV});
-        browser.storage.local.set({PoM});
-        browser.storage.local.set({MonitorId});
-        browser.storage.local.set({Period});
-        browser.storage.local.set({PoMsig});
-        */
-
-
-        // storing of something
-        /*
-        let results = browser.storage.local.get(data);
-        results.then(function(result) {
-            console.log(result);
-        });
-
-        browser.storage.local.set({data: {STHs: "test_sth_test"}});
-        let results2 = browser.storage.local.get(data);
-        results2.then(function(result) {
-            console.log(result);
-        });*/
+    // check sths
+    console.log("The following is the STH(s) from period 3:")
+    console.log(sths3);
+});
