a986bfa57b8adc97b3a464bbaf861a6c4626439f
Kevin Freyberg <kfreyberg01@gmail.com>
2023-04-12 21:44:37 -0400

    Update certificate_checks.js

--- src/certificate_checks.js
+++ src/certificate_checks.js
@@ -1,55 +1,13 @@
 class CertificateCheck {
 
     //Step 1: Signature Verification on monitor update
+    static checkUpdate(period) {
+        let gettingItem0 = browser.storage.local.get([`sths${period}`, `revs${period}`, `accs${period}`, `cons${period}`]);
+        // gettingItem.then((data) => {
+        // }
+    }
 
     //Step 2: Check if CA and Loggers are in CONs and ACCs PoM lists
-    // onGot(data) {
-    //     var certCA = data.cert.Issuer
-    //     var loggers = ["localhost:9000","localhost:9001", "localhost:9002"]
-      
-    //     var conLoggerCount = 0
-    //     var accLoggerCount = 0
-      
-    //     var conPOMs = []
-    //     for (var i = 0; i < data.cons1.length; i++) {
-    //       conPOMs.push(data.cons1[i].payload[0])
-    //       if (loggers.includes(data.cons1[i].payload[0])) {
-    //         conLoggerCount += 1;
-    //       }
-    //     }
-        
-    //     var accPOMs = []
-    //     for (var i = 0; i < data.accs1.length; i++) {
-    //       accPOMs.push(data.accs1[i].payload[0])
-    //       if (loggers.includes(data.accs1[i].payload[0])) {
-    //         accLoggerCount += 1;
-    //       }
-    //     }
-      
-    //     var loggerCountCheck = (conLoggerCount == 0 && accLoggerCount == 0)
-      
-    //     if (conPOMs.length == 0 && accPOMs.length == 0) {
-    //         //return 1; //success
-    //         console.log("Pass.")
-    //     }
-    //     else if ((!conPOMs.includes(certCA) && !accPOMs.includes(certCA)) && loggerCountCheck) {
-    //         //return 1; //success
-    //         console.log("Pass.")
-    //     }
-    //     else if ((conPOMs.includes(certCA) || accPOMs.includes(certCA)) && loggerCountCheck) {
-    //         //return 0; //fail
-    //         console.log("Fail.")
-    //     }
-    //     else {
-    //         //return 0; //fail
-    //         console.log("Fail.")
-    //     }
-    //   }
-      
-    //     onError(error) {
-    //     console.log(`Error: ${error}`);
-    //   }
-        
     static checkPOMs() {
         let gettingItem = browser.storage.local.get(["cons1", "accs1", "cert"]);
         gettingItem.then((data) =>
